274fb83c9bc4ccab5535c24c08e3f99c0406b7af
loubot5000 <104558799+loubot5000@users.noreply.github.com>
2022-06-20 11:53:44 +0100

    uiqhw
    hfpud

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1',
-        user: 'postgres',
-        password: 'test',
+        user: 'root',
+        password: '',
         database: 'loginformytvideo'
     }
 })
